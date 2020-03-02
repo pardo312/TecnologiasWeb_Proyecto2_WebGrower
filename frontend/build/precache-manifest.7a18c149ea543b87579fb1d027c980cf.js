ab76ca8935a3215236f03dcf034c351127b3c01a
pardo312 <42390277+pardo312@users.noreply.github.com>
2020-03-02 09:35:03 -0500

    a

---
+++ frontend/build/precache-manifest.7a18c149ea543b87579fb1d027c980cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763f53d676667225436d98f42f16df2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd5016d9cb5db020ce68",
+    "url": "/static/js/2.2fb642a0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2fb642a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1297b9205e819935edb5",
+    "url": "/static/js/main.a079e6a5.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  },
+  {
+    "revision": "6e64ac661ab707c27d5c88bd5bc1ff8b",
+    "url": "/static/media/background.6e64ac66.jpg"
+  },
+  {
+    "revision": "66e04fee54d1534269457a3a64ab4d0e",
+    "url": "/static/media/logo.66e04fee.png"
+  }
+]);
